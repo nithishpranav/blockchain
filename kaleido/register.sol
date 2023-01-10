1c069080e8b3cccad2588a6354a238478ab594ef
nithishpranav <nithishpranavnp@gmail.com>
2023-01-10 14:07:32 +0530

    made changes

--- kaleido/register.sol
+++ kaleido/register.sol
@@ -5,13 +5,18 @@ pragma solidity ^0.8.0;
 
 contract register{
 
-    mapping(string => address) public walletHash;
+    mapping(bytes32 => address) public walletHash;
 
-    function registerWallet(string memory _hash, address _wallet) public{
+    function registerWallet(string memory _gstId, string memory _password, address _wallet) public returns(bytes32){
+        
+        bytes32 _hash = keccak256(abi.encode(_gstId,_password));
         walletHash[_hash] = _wallet;
+        return _hash;
     } 
+    
+    function getUser(string memory _gstId, string memory _password) public view returns(address){
+        bytes32 _hash = keccak256(abi.encode(_gstId,_password));
 
-    function getUser(string memory _hash) public view returns (address){
         return walletHash[_hash];
     }
     
