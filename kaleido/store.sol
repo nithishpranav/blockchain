76c64434c0d119982a295c77bbe2ac542f089a6c
nithishpranav <nithishpranavnp@gmail.com>
2023-01-17 10:42:32 +0530

    changes made

--- kaleido/store.sol
+++ kaleido/store.sol
@@ -7,11 +7,20 @@ contract store{
 
     mapping (address => bytes32) public dataStore;
 
+
+    event userStore(
+        address userWalletAddress
+    );
+
     function set(address walletAddress, bytes32 dataHash ) public{
         dataStore[walletAddress] = dataHash;
+        emit userStore(walletAddress);
     }
 
-    function query(address walletAddress) public view returns (bytes32){
+    function query(address walletAddress) public  returns (bytes32){
+        
+        //emit is used fot event stream
+        emit userStore(walletAddress);
         return dataStore[walletAddress];
     }
 
