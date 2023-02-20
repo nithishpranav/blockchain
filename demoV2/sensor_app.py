34db85d944caeb063c2f2ebf6ad2b733f5c3991a
nithishpranav <nithishpranavnp@gmail.com>
2023-02-20 13:32:44 +0530

    docker setup

--- demoV2/sensor_app.py
+++ demoV2/sensor_app.py
@@ -9,6 +9,7 @@ from datetime import datetime
 import csv
 from pathlib import Path
 import pathlib
+import os
 
 
 import time
@@ -168,4 +169,5 @@ def disconnect():
     print('Client disconnected',  request.sid)
 
 if __name__ == '__main__':
-    socketio.run(app)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
